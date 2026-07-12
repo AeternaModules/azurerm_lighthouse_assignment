29e3ceec6cca199bf63f8993a78fc3c7e9bc22d5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "lighthouse_assignments_id" {
+  description = "Map of id values across all lighthouse_assignments, keyed the same as var.lighthouse_assignments"
+  value       = { for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : k => v.id }
+}
 output "lighthouse_assignments_lighthouse_definition_id" {
   description = "Map of lighthouse_definition_id values across all lighthouse_assignments, keyed the same as var.lighthouse_assignments"
   value       = { for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : k => v.lighthouse_definition_id }
