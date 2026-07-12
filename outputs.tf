8aecfa4a10209dc9b6348321a7c606e6868ca0e9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "lighthouse_assignments" {
-  description = "All lighthouse_assignment resources"
-  value       = azurerm_lighthouse_assignment.lighthouse_assignments
-}
 output "lighthouse_assignments_lighthouse_definition_id" {
-  description = "List of lighthouse_definition_id values across all lighthouse_assignments"
-  value       = [for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : v.lighthouse_definition_id]
+  description = "Map of lighthouse_definition_id values across all lighthouse_assignments, keyed the same as var.lighthouse_assignments"
+  value       = { for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : k => v.lighthouse_definition_id }
 }
 output "lighthouse_assignments_name" {
-  description = "List of name values across all lighthouse_assignments"
-  value       = [for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : v.name]
+  description = "Map of name values across all lighthouse_assignments, keyed the same as var.lighthouse_assignments"
+  value       = { for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : k => v.name }
 }
 output "lighthouse_assignments_scope" {
-  description = "List of scope values across all lighthouse_assignments"
-  value       = [for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : v.scope]
+  description = "Map of scope values across all lighthouse_assignments, keyed the same as var.lighthouse_assignments"
+  value       = { for k, v in azurerm_lighthouse_assignment.lighthouse_assignments : k => v.scope }
 }
 
